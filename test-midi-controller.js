9695eb552961cf4370a9a31d3ba498fc218aaa5e
jgaginis <65040337+jgaginis@users.noreply.github.com>
2025-06-29 18:01:50 -0400

    Update test-midi-controller.js

--- test-midi-controller.js
+++ test-midi-controller.js
@@ -1,4 +1,4 @@
-//midi-controller.js
+//test-midi-controller.js
 let midiOutput = null;
 
 navigator.requestMIDIAccess().then(access => {
@@ -10,7 +10,7 @@ navigator.requestMIDIAccess().then(access => {
   }
 });
 
-function sendNote(channel = 0, pitch = Math.floor(Math.random() * (84 - 36 + 1)) + 36, velocity = Math.floor(Math.random() * (72 - 32 + 1)) + 32 {
+function sendNote(channel = 0, pitch = Math.floor(Math.random() * ((84 - 36 + 1) + 36))), velocity = Math.floor(Math.random() * ((72 - 32 + 1) + 32)) {
   if (!midiOutput) return;
   midiOutput.send([0x90 + channel, pitch, velocity]); // Note On
   setTimeout(() => {
