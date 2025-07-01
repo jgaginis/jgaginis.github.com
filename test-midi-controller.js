e3d55909e56dd6ba52857ffec4342b26492d371f
jgaginis <65040337+jgaginis@users.noreply.github.com>
2025-07-01 15:33:02 -0400

    Update test-midi-controller.js

--- test-midi-controller.js
+++ test-midi-controller.js
@@ -1,7 +1,7 @@
 //test-midi-controller.js
 const times = 32;//const delay = 375; // milliseconds between notes
 const pitchArray = createRandomPitchArray(6, 32, 85);
-const rhythmPattern = [205, 136, 222, 136, 205, 375];
+const rhythmPattern = [205, 136, 222, 136, 205, 410];
 
 let midiOutput = null;
 
