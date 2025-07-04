1fe9e0df2523c84fe1b5b8a5808c6401251affcb
jgaginis <65040337+jgaginis@users.noreply.github.com>
2025-07-03 22:26:56 -0400

    Update test-midi-controller.js

--- test-midi-controller.js
+++ test-midi-controller.js
@@ -58,6 +58,8 @@ document.getElementById("noteArp").addEventListener("click", () => {
   sendArp(times, delay, pitchArray);
 });
 
+
+
 function sendNote(channel = 0, pitch = randRange(32, 85), velocity = randRange(32, 73)) {
   if (!midiOutput) return;
   midiOutput.send([0x90 + channel, pitch, velocity]); // Note On
@@ -84,6 +86,42 @@ function sendProgramChange(channel = 0, programNum = Math.floor(Math.random() * 
 
 //this automatically generates a thirty bar arpeggio but it needs to be a function that is triggered by a button.  maybe it should be an array that also only generates four notes? work on this tomorrow? 
 
+function sendChordWithSustain() {
+  const basePitch = pitchArray[Math.floor(Math.random() * pitchArray.length)];
+
+  // Choose dyad or triad
+  const chordType = Math.random() < 0.5 ? 2 : 3;
+
+  // Harmony intervals: could be 3rd, 5th, 7th
+  const intervals = [0, 4, 7, 10]; // major 3rd, perfect 5th, minor 7th
+  const selected = [];
+
+  while (selected.length < chordType) {
+    const interval = intervals[Math.floor(Math.random() * intervals.length)];
+    const note = basePitch + interval;
+    if (!selected.includes(note)) selected.push(note);
+  }
+
+  // Sustain CC (64), value from 108 to 127
+  const sustainValue = randRange(108, 128);
+  output.send([0xB0, 64, sustainValue]); // 0xB0 = CC on channel 1
+
+  // Send chord notes
+  selected.forEach(pitch => {
+    output.send([0x90, pitch, 100]); // Note On
+  });
+
+  // Release chord after long hold
+  setTimeout(() => {
+    selected.forEach(pitch => {
+      output.send([0x80, pitch, 0]); // Note Off
+    });
+    output.send([0xB0, 64, 0]); // Turn sustain pedal off
+  }, 4000); // 4 seconds
+  
+  document.getElementById("send-chord").addEventListener("click", sendChordWithSustain);
+
+}
 
 
 
